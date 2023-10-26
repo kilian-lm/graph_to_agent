7b7b54be2b74f378d03868898e463fc401c1c6d8
kilian-lm <58375894+kilian-lm@users.noreply.github.com>
2023-10-26 19:51:25 +0200

    added agent pool logic, but missing all functionalities, tooo tiered today

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 # Include necessary imports for your Solver class
+# For example:
+# import requests, json, logging
 
 app = Flask(__name__)
 
@@ -10,9 +12,16 @@ def index():
 @app.route('/solve', methods=['POST'])
 def solve():
     data = request.json
+
     # Instantiate your Solver class and call the appropriate methods
-    # Here, I'm just returning the data for simplicity
-    return jsonify(data)
+    solver = Solver(openai_api_key="YOUR_API_KEY", url="YOUR_URL")
+    problem_description = data.get("problem_description", "")
+    responses = solver.model_problem_spaces(problem_description)
+
+    # Convert responses into a format suitable for vis.js
+    # This step might need additional details on how you want to visualize the results.
+
+    return jsonify(responses)
 
 if __name__ == "__main__":
     app.run(debug=True)
