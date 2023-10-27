462186bd95a487880ffa5bf9e8597c1d9df617f4
kilian-lm <kilian@familylehn.com>
2023-10-28 00:53:31 +0200

    changes server side

--- app/app.py
+++ app/app.py
@@ -10,9 +10,9 @@ from dotenv import load_dotenv
 from google.api_core.exceptions import NotFound
 import logging
 
+from Solver import Solver
 
 logging.basicConfig(level=logging.DEBUG)
-
 
 load_dotenv()
 
@@ -96,7 +96,8 @@ class AgentModelerApp:
         """Create a table with the necessary schema."""
         schema = [
             bigquery.SchemaField("timestamp", "TIMESTAMP", mode="REQUIRED"),
-            bigquery.SchemaField("data", "STRING", mode="REQUIRED"),
+            bigquery.SchemaField("problem_description", "STRING", mode="REQUIRED"),
+            bigquery.SchemaField("agent_interactions", "STRING", mode="REPEATED"),
         ]
         table = bigquery.Table(table_id, schema=schema)
         try:
@@ -104,6 +105,46 @@ class AgentModelerApp:
             logging.info(f"Table {table_id} created successfully.")
         except Exception as e:
             logging.error(f"Error creating table {table_id}: {e}")
+
+    def save_to_bigquery(self, solver: Solver, problem_description: str):
+        """Saves Solver interactions to BigQuery."""
+        table_id = f"{self.project_id}.graph_to_agent.graph_to_agent_20231027"
+
+        # Check if table exists, if not, create it
+        if not self._table_exists(table_id):
+            self._create_table(table_id)
+
+        # Convert datetime objects to string
+        timestamp_str = datetime.utcnow().isoformat()
+
+        rows_to_insert = [{
+            u"timestamp": timestamp_str,
+            u"problem_description": problem_description,
+            u"agent_interactions": json.dumps(solver.agents),
+        }]
+
+        errors = self.bigquery_client.insert_rows_json(table_id, rows_to_insert)
+        if errors == []:
+            return jsonify({"message": "Saved to BigQuery successfully"})
+        else:
+            return jsonify({"message": f"Encountered errors: {errors}"})
+
+    def _create_solver_setup(self, data) -> dict:
+        """Process data to the right format for a Solver object."""
+        return {entry['agent']: entry['messages'] for entry in data['agent_interactions']}
+
+    def create_solver_from_bigquery(self, table_id: str) -> Solver:
+        """Instantiate a Solver object from the most recent setup in BigQuery"""
+        query = f"""
+          SELECT data
+          FROM `{table_id}`
+          ORDER BY timestamp DESC
+          LIMIT 1
+          """
+        results = self.bigquery_client.query(query).result()
+        for row in results:
+            agents_setup = self._create_solver_setup(json.loads(row.data))
+            return Solver(openai_api_key="YOUR_OPENAI_API_KEY", url="YOUR_URL", agents=agents_setup)
 
     def run(self):
         self.app.run(debug=True)
