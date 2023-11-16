0327d191539c02d089aae9a5e16620ef3067a9b8
kilian-lm <58375894+kilian-lm@users.noreply.github.com>
2023-11-16 11:26:54 +0100

    struggeling with checkpointing

--- sql_queries/nodes_query.py
+++ sql_queries/nodes_query.py
@@ -1,4 +1,4 @@
 # SELECT * FROM `enter-universes.graph_to_agent.edges_table` where graph_id = "20231114181549"
 
 
-NODES_QUERY = """SELECT * FROM `{dataset_id}.nodes_table` WHERE graph_id = '{graph_id}'"""
+NODES_QUERY = """SELECT * FROM `{tbl_ref}` WHERE graph_id = '{graph_id}'"""
