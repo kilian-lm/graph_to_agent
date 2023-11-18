e580964cf6943acf5220d33d51dec8ad4a30458d
kilian-lm <58375894+kilian-lm@users.noreply.github.com>
2023-11-18 21:30:11 +0100

    still stuck on correct recursiv gpt call building, cleaning controllers and adapt even more modular apporach

---
+++ controllers/MatchedGPTCallsHandler.py
@@ -0,0 +1,14 @@
+class MatchedGPTCallsHandler:
+    def __init__(self, logger, openai_api_key, openai_base_url, headers):
+        # Initialization and setup for handling matched GPT calls
+
+    def process_matched_gpt_calls(self, matched_calls, sorted_components):
+        # Process matched GPT calls
+
+    def update_gpt_call_with_responses(self, gpt_call, var_responses):
+        # Update GPT call with responses
+
+    def get_next_variable(self, current_var):
+        # Get the next variable
+
+    # Any other recursive or matched call specific methods
