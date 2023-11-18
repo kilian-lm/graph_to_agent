098fe9a4a51a05cf9a03cb5ec5a84345cb983d86
kilian.lehn <kilian.lehn@annalect.com>
2023-11-18 22:11:21 +0100

    still stuck on correct recursiv gpt call building, cleaning controllers and adapt even more modular apporach

--- controllers/MatchedGPTCallsHandler.py
+++ controllers/MatchedGPTCallsHandler.py
@@ -1,14 +1,71 @@
 class MatchedGPTCallsHandler:
-    def __init__(self, logger, openai_api_key, openai_base_url, headers):
-        # Initialization and setup for handling matched GPT calls
+    def __init__(self, timestamp, matrix_dataset_id, graph_dataset_id):
+        try:
+            self.timestamp = timestamp
+            self.log_file = f'{self.timestamp}_matrix_layer_two.log'
+            self.log_dir = './temp_log'
+            self.log_level = logging.DEBUG
+            self.logger = CustomLogger(self.log_file, self.log_level, self.log_dir)
 
-    def process_matched_gpt_calls(self, matched_calls, sorted_components):
-        # Process matched GPT calls
+            self.openai_api_key = os.getenv('OPENAI_API_KEY')
+            self.openai_base_url = "https://api.openai.com/v1/chat/completions"
+            self.headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f'Bearer {self.openai_api_key}'
+            }
 
-    def update_gpt_call_with_responses(self, gpt_call, var_responses):
-        # Update GPT call with responses
+            self.gpt_call_log = []
 
-    def get_next_variable(self, current_var):
-        # Get the next variable
+            self.matrix_dataset_id = matrix_dataset_id
+            self.graph_dataset_id = graph_dataset_id
+            self.bq_handler = BigQueryHandler(self.timestamp, self.graph_dataset_id)
 
-    # Any other recursive or matched call specific methods
+            # Additional attributes specific to matched GPT calls can be initialized here
+        except json.JSONDecodeError as e:
+            self.logger.error(f"Failed to parse BQ_CLIENT_SECRETS environment variable: {e}")
+            raise
+        except Exception as e:
+            self.logger.error(f"An error occurred while initializing: {e}")
+            raise
+
+    def process_calls(self, matched_calls):
+        # Implement logic to process matched GPT calls
+        for gpt_call in matched_calls:
+            try:
+                updated_call, response = self.process_single_gpt_call(gpt_call)
+                self.log_gpt_call(updated_call, response)
+            except Exception as e:
+                self.logger.error(f"Error processing matched GPT call: {e}")
+
+    def process_single_gpt_call(self, gpt_call):
+        # Logic to process a single matched GPT call
+        # This might involve more complex logic specific to matched calls
+        # For example, handling recursive calls or variable replacements
+        updated_call = self.update_gpt_call_with_responses(gpt_call)
+        response = self.get_gpt_response(updated_call)
+        return updated_call, response
+
+    def update_gpt_call_with_responses(self, gpt_call):
+        # Logic to update the GPT call (e.g., replacing placeholders with actual values)
+        # This method might be more complex in reality, depending on the application's needs
+        return gpt_call  # Placeholder for actual implementation
+
+    def log_gpt_call(self, request, response):
+        log_entry = {"request": request, "response": response}
+        self.gpt_call_log.append(log_entry)
+        self.log_info(log_entry)
+
+    def log_info(self, message):
+        self.logger.info(message)
+
+    def get_gpt_response(self, processed_data):
+        self.logger.debug(processed_data)
+        response = requests.post(self.openai_base_url, headers=self.headers, json=processed_data)
+        self.logger.info(response)
+        if response.status_code == 200:
+            self.logger.info(response.json()["choices"][0]["message"]["content"])
+            return response.json()["choices"][0]["message"]["content"]
+        else:
+            raise Exception(f"Error in GPT request: {response.status_code}, {response.text}")
+
+    # Other methods specific to handling matched GPT calls can be added as needed
